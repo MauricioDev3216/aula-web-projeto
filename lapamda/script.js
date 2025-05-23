51c4914cd3804b62c37ca2d4d4dcc4de2f5336e5
Gustavokvs <gustavokruger514@gmail.com>
2025-05-23 15:03:20 -0300

    terceiro

--- lapamda/script.js
+++ lapamda/script.js
@@ -8,16 +8,13 @@ let vezesAcesa = 0;
 botao.addEventListener('click', () => {
     ligada = !ligada;
 
-    // Atualiza a imagem e o texto do botão
     lampada.src = ligada ? 'ligada.png' : 'desligada.png';
     botao.textContent = ligada ? 'Desligar' : 'Ligar';
 
-    // Incrementa o contador apenas quando a luz é ligada
     if (ligada) {
         vezesAcesa++;
         contador.textContent = `A luz foi acesa ${vezesAcesa} vezes.`;
     }
 
-    // Alterna a classe do body para mudar o fundo
     body.classList.toggle('luz-ligada', ligada);
 });
